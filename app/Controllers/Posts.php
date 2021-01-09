23cb8b6f29391870262ba29eec0b5ae12ab56a3b
Gun Gun Priatna <gungunpriatna02@gmail.com>
2021-01-09 19:10:05 +0700

    show detail post

--- app/Controllers/Posts.php
+++ app/Controllers/Posts.php
@@ -13,4 +13,18 @@ class Posts extends ResourceController
     {
         return $this->respond($this->model->findAll());
     }
+
+    public function show($id = null)
+    {
+        $record = $this->model->find($id);
+        if (!$record) {
+            # code...
+            return $this->failNotFound(sprintf(
+                'post with id %d not found',
+                $id
+            ));
+        }
+
+        return $this->respond($record);
+    }
 }
